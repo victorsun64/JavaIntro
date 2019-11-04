d4100acb8ad6b9ac8cb44c75c13bd715d44ab921
victor.sun <victor.sun@AMS-GYM-EDV1-04.gym.ams.local>
2019-11-04 18:16:53 +0100

    JavaBasics hinzugefügt

---
+++ src/javaBasics/JavaBasics.java
@@ -0,0 +1,38 @@
+package javaBasics;
+
+public class JavaBasics {
+
+	public static void main(String[] args) {
+		System.out.println("Text");
+		
+		//Kommentar Text einzeilig
+		
+		/*mehrzeiligen
+		 * Kommentar
+		 */
+		
+		// Unterschied zwischen print und println
+		System.out.print("Hallo");
+		System.out.print(" ");
+		System.out.print("du");
+		System.out.print(" ");
+		System.out.println("!");
+		//
+		// Textverkettung
+		System.out.println("Hallo" + " " + "du" + " " + "!");
+		//
+		System.out.println(120);
+		System.out.println(1);
+		System.out.println(2);
+		//
+		System.out.println(123.45);
+		//
+		//System.out.println(123,45);
+		// Rechnen
+		System.out.println(2+2);
+		System.out.println(4-3);
+		System.out.println(3*3);
+		System.out.println(9/3);
+	}
+
+}
