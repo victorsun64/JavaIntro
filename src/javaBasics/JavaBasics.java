82ef621a5fdea9c6059c06928223151d96a8a8d6
victor.sun <victor.sun@AMS-GYM-EDV1-04.gym.ams.local>
2019-11-04 18:30:13 +0100

    Weitere Zeilen hinzugefügt

--- src/javaBasics/JavaBasics.java
+++ src/javaBasics/JavaBasics.java
@@ -33,6 +33,21 @@ public class JavaBasics {
 		System.out.println(4-3);
 		System.out.println(3*3);
 		System.out.println(9/3);
+		// Rechnen #2 - Dezimalstellen
+		System.out.println(8/3);
+		System.out.println(8.0/3);
+		System.out.println(8/3.0);
+		System.out.println(9/3.0);
+		// F oder D verwenden
+		System.out.println(9/3F);
+		System.out.println(9D/3);
+		//
+		System.out.println(1+1+"Ergebnis");
+		System.out.println("1+1="+1+1);
+		//
+		System.out.println((1+1)+"Ergebnis");
+		System.out.println("1+1="+(1+1));
+		
 	}
 
 }
