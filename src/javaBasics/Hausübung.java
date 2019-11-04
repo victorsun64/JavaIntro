d960e08b9d1065a54280595b37df0f883054e26e
victorsun <victorsun@DESKTOP-4N9QROP>
2019-11-04 22:32:34 +0100

    HÜ hinzugefügt

---
+++ src/javaBasics/Hausübung.java
@@ -0,0 +1,10 @@
+package javaBasics;
+
+public class Hausübung {
+
+	public static void main(String[] args) {
+		System.out.println("Dies" + " " + "habe" + " " + "ich" + " " + "zuhause" + " " + "gemacht" + "!");
+
+	}
+
+}
