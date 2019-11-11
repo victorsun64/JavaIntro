edf97a2d6699827038f22b5184f64db1338b4477
victor.sun <victor.sun@AMS-GYM-EDV1-04.gym.ams.local>
2019-11-11 17:36:25 +0100

    long,float,double hinzugefügt

--- src/javaBasics/VariablenBasics.java
+++ src/javaBasics/VariablenBasics.java
@@ -31,6 +31,24 @@ public class VariablenBasics {
 		System.out.println(Integer.MIN_VALUE);
 		System.out.println(Integer.MAX_VALUE);
 		
+		// long - 8 byte
+		long grosse_zahl = 1234567890;
+		System.out.println(grosse_zahl);
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		
+		// float
+		float kleine_komma_zahl = 123.456F; // F ... Float
+		System.out.println(kleine_komma_zahl);
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Float.MAX_VALUE);
+		
+		// double
+		double grosse_komma_zahl = 123456789.0987654321D;
+		System.out.println(grosse_komma_zahl);
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE);		
+		
 	}
 
 }
