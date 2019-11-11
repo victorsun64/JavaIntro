794c7e0129a11c86cf8264e615d1d0beba5fbcfe
victor.sun <victor.sun@AMS-GYM-EDV1-04.gym.ams.local>
2019-11-11 18:04:12 +0100

    SchleifenBasics

---
+++ src/javaBasics/SchleifenBasics.java
@@ -0,0 +1,12 @@
+package javaBasics;
+
+public class SchleifenBasics {
+
+	public static void main(String[] args) {
+		
+		// FOR Schleife
+		for (int i = 0; i < 5; i =i+1)
+			System.out.println(i);
+	}
+
+}
