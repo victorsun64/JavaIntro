7961291a4c6a7ee147df7ef53e1280b844808148
victor.sun <victor.sun@AMS-GYM-EDV1-04.gym.ams.local>
2019-11-11 18:49:51 +0100

    EinMalEins

---
+++ src/javaBasics/EinMalEins.java
@@ -0,0 +1,14 @@
+package javaBasics;
+
+public class EinMalEins {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i = i+ 1) {
+			for (int j = 1; j <= 10; j =j + 1) {
+				System.out.println(j + "x" + i + "=" + (i * j));
+	        }
+
+	}
+	}
+
+}
