be22aa823fd1901c045293e81f71298aa5f93e5d
victor.sun <victor.sun@AMS-GYM-EDV1-04.gym.ams.local>
2019-11-18 17:45:12 +0100

    Bedingung bis zum Punkt Klammern geupdated

--- src/javaBasics/Bedingung.java
+++ src/javaBasics/Bedingung.java
@@ -8,10 +8,47 @@ public class Bedingung {
 			System.out.println("1 ist kleiner als 3");
 		}
 		//
-		if (3<1) {
-			System.out.println("3 ist kleiner als 1");
+//		if (3<1) {
+//			System.out.println("3 ist kleiner als 1");
+//		}
+		//
+		int zahlA;
+		int zahlB;
+		//
+		zahlA = 3;
+		zahlB = 1;
+		//
+		int zahlC = 5, zahlD = 4;
+		//
+		if (zahlA < zahlB) {
+			System.out.println(zahlA + " ist kleiner als " + zahlB);
+		} else {
+			System.out.println(zahlA + " ist nicht kleiner als " + zahlB);
 		}
-
+		// abfrage auf gleich ==
+		if (zahlA == zahlB) {	
+		}
+		// nicht gleich
+		if (zahlA != zahlB) {	
+		}
+		//
+		if ("hans" == "hans") {
+			System.out.println("Hans");
+		}
+		String nameA = "hans";
+		String nameB = "hans";
+		//
+		if (nameA == nameB) {
+			System.out.println(nameA);
+		}
+		// alte Variante String Vergleich
+		if ("hansi".contentEquals("hansi")) {
+			System.out.println("hansi");
+		}
+		// Klammern
+		if (3<1)
+			System.out.println("juhu");
+			System.out.println("abc");
 	}
 
 }
