9d4caac77a0abeadd1dcc44afcb18ac8024468f0
victor.sun <victor.sun@AMS-GYM-EDV1-04.gym.ams.local>
2019-11-18 18:02:54 +0100

    OddNumbers aufgabe gemacht

---
+++ src/aufgaben/OddNumbers.java
@@ -0,0 +1,10 @@
+package aufgaben;
+
+public class OddNumbers {
+
+	public static void main(String[] args) {
+		for (int UngeradeZahl = 1; UngeradeZahl <= 100; UngeradeZahl = UngeradeZahl + 2) {
+			System.out.println(UngeradeZahl);
+	}
+	}
+}
