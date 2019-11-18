c9a26b07e2b60f872695f9717a46032707314d39
victor.sun <victor.sun@AMS-GYM-EDV1-04.gym.ams.local>
2019-11-18 17:23:02 +0100

    Bedingung zwischensave

--- src/javaBasics/Bedingung.java
+++ src/javaBasics/Bedingung.java
@@ -3,7 +3,14 @@ package javaBasics;
 public class Bedingung {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 
+		if (1<3) {
+			System.out.println("1 ist kleiner als 3");
+		}
+		//
+		if (3<1) {
+			System.out.println("3 ist kleiner als 1");
+		}
 
 	}
 
