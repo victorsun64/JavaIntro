b722affea21a09a73b703b709bbbd639f93e15d5
victor.sun <victor.sun@AMS-GYM-EDV1-04.gym.ams.local>
2019-11-18 17:19:42 +0100

    Bedingung hinzugefügt

---
+++ src/javaBasics/Bedingung.java
@@ -0,0 +1,10 @@
+package javaBasics;
+
+public class Bedingung {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/javaBasics/EinMalEins.java
+++ src/javaBasics/EinMalEins.java
@@ -5,7 +5,7 @@ public class EinMalEins {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 10; i = i+ 1) {
 			for (int j = 1; j <= 10; j =j + 1) {
-				System.out.println(i + "x" + j + "=" + (i * j));
+				System.out.println(j + "x" + i + "=" + (i * j));
 	        }
 
 	}
