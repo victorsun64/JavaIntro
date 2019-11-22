0325c0a5e1450cd134ba759a7a34cc0912234a33
victorsun <victorsun@DESKTOP-4N9QROP>
2019-11-22 22:25:50 +0100

    Additionsrechner vollendet

--- src/aufgaben/AdditionsRechner.java
+++ src/aufgaben/AdditionsRechner.java
@@ -1,9 +1,17 @@
 package aufgaben;
 
+import java.util.Scanner;
+
 public class AdditionsRechner {
 
-	public static void main(String[] args) {
-		
-	}
-
-}
+public static void main(String[] args) {
+int zahl1,zahl2,ergebnis;
+Scanner scanner = new Scanner(System.in);
+//
+System.out.println("Geben Sie bitte die erste Zahl ein:");
+zahl1=scanner.nextInt();
+System.out.println("Geben Sie bitte die zweite Zahl ein:");
+zahl2=scanner.nextInt();
+ergebnis=zahl1+zahl2;
+System.out.println("Das Ergebnis ist: " + ergebnis);
+}}
