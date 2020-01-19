617b755eb8ae0277e4ebde26203d9862bf2b2600
victorsun <victorsun@DESKTOP-4N9QROP>
2020-01-19 23:15:23 +0100

    Taschenrechner vollendet

--- src/aufgaben/TaschenRechner.java
+++ src/aufgaben/TaschenRechner.java
@@ -4,18 +4,25 @@ import java.util.Scanner;
 
 public class TaschenRechner {
 
-public static void main(String[] args) {
-		long zahl1,zahl2;
-		String ergebnis;
+private static Scanner scanner;
+	
+	public static void main(String[] args) {
+		long zahl1,zahl2,ergebnis;
 		String operator;
-		Scanner scanner = new Scanner(System.in);
-		//
+		scanner = new Scanner(System.in);
 		System.out.println("Geben Sie bitte die erste Zahl ein:");
 		zahl1=scanner.nextLong();
 		System.out.println("Geben Sie bitte die zweite Zahl ein:");
 		zahl2=scanner.nextLong();
 		System.out.println("Geben Sie bitte den Operator ein:");
-		operator=scanner.toString();
-		ergebnis=zahl1+operator+zahl2;
-		System.out.println("Das Ergebnis ist: " + ergebnis);
+		operator=scanner.next();
+		if ("+".equals(operator))
+		{ergebnis=zahl1+zahl2;System.out.println("Das Ergebnis ist: " + ergebnis);}
+		else if("-".equals(operator))
+		{ergebnis=zahl1-zahl2;System.out.println("Das Ergebnis ist: " + ergebnis);}
+		else if("/".equals(operator))
+		{ergebnis=zahl1/zahl2;System.out.println("Das Ergebnis ist: " + ergebnis);}
+		else if("*".equals(operator))
+		{ergebnis=zahl1*zahl2;System.out.println("Das Ergebnis ist: " + ergebnis);}
+		else {System.out.println("ERR0R");};
 		}}
