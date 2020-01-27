4d1475324adb1de851f431b80b1c9c19354cb3f9
victor.sun <victor.sun@AMS-GYM-EDV1-04.gym.ams.local>
2020-01-27 18:49:09 +0100

    bubblesort noch nicht fertig!!!

--- src/aufgaben/Bubblesort.java
+++ src/aufgaben/Bubblesort.java
@@ -1,11 +1,20 @@
 package aufgaben;
 
+import java.util.ArrayList;
+
 public class Bubblesort {
 
 	public static void main(String[] args) {
-	int i,t;
-	int [] zahlen = {9,1,5,7,4,6,3,2,8,0};
-	for (i=0;i<zahlen.length;i++);
-	if (zahlen[i]<zahlen[i+1]) {t=zahlen[i];zahlen[i+1]=zahlen[i];zahlen[i+1]=t;};
-	System.out.println(zahlen);
-}}
+		int i, n, t = 0;
+		int[] zahlen = { 9, 1, 5, 7, 4, 6, 3, 2, 8, 0 };
+		for (i = zahlen.length; i > 1; --i) {
+			for (n = 0; i < n - 1; n++) {
+				if (zahlen[i] > zahlen[i + 1]) {
+					zahlen[i] = t;
+					zahlen[i + 1] = zahlen[i];
+					t = zahlen[i + 1];
+				}
+			}
+		}System.out.println(zahlen);
+	}
+}
