db23859ddc6db0290dd9288ff91468410a09c3df
victorsun <victorsun@DESKTOP-4N9QROP>
2020-02-15 16:12:12 +0100

    Bubblesort fertig

--- src/aufgaben/Bubblesort.java
+++ src/aufgaben/Bubblesort.java
@@ -3,9 +3,22 @@ package aufgaben;
 public class Bubblesort {
 
 	public static void main(String[] args) {
-	int i,t;
-	int [] zahlen = {9,1,5,7,4,6,3,2,8,0};
-	for (i=0;i<zahlen.length;i++);
-	if (zahlen[i]<zahlen[i+1]) {t=zahlen[i];zahlen[i+1]=zahlen[i];zahlen[i+1]=t;};
-	System.out.println(zahlen);
-}}
+		int[] zahlen = { 9, 1, 5, 7, 4, 6, 3, 2, 8, 0 };
+		int n = zahlen.length;
+		int t = 0;
+		int i, j;
+		for (i = 1; i < n; i++) {
+			for (j = 0; j < (n-i); j++) {
+				if (zahlen[j] > zahlen[j + 1]) {
+					t = zahlen[j];
+					zahlen[j] = zahlen[j + 1];
+					zahlen[j + 1] = t;
+				}
+			}
+		}
+		for (i = 0; i < zahlen.length; i++) {
+			System.out.print(zahlen[i] + " ");
+		}
+		System.out.println();
+	}
+}
