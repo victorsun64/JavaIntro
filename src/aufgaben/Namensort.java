f8a1190db8661011fe2aca0ded4d9dcdf3510dbc
victor.sun <victor.sun@AMS-GYM-EDV1-04.gym.ams.local>
2020-02-17 17:19:55 +0100

    Jetzt mit Namen aus der Klasse

--- src/aufgaben/Namensort.java
+++ src/aufgaben/Namensort.java
@@ -7,21 +7,26 @@ public class Namensort {
 	public static void main(String[] args) {
 		String t;
 		int i, j;
-		ArrayList<String> cars = new ArrayList<String>();
-		cars.add("Volvo");
-		cars.add("BMW");
-		cars.add("Ford");
-		cars.add("Mazda");
-		int n = cars.size();
+		ArrayList<String> klasse = new ArrayList<String>();
+		klasse.add("Basti");
+		klasse.add("Viktor");
+		klasse.add("Chemmy");
+		klasse.add("Oleg");
+		klasse.add("David");
+		klasse.add("Theo");
+		klasse.add("Leo");
+		klasse.add("Martin");
+		klasse.add("Martina");
+		int n = klasse.size();
 		for (i = 1; i < n; i++) {
 			for (j = 0; j < (n - i); j++) {
-				if (cars.get(j).compareToIgnoreCase(cars.get(j + 1)) > 0) {
-					t = cars.get(j);
-					cars.set(j,cars.get(j+1));
-					cars.set(j+1,t);
+				if (klasse.get(j).compareToIgnoreCase(klasse.get(j + 1)) > 0) {
+					t = klasse.get(j);
+					klasse.set(j,klasse.get(j+1));
+					klasse.set(j+1,t);
 				}
 			}
-		}System.out.println(cars);
+		}System.out.println(klasse);
 	}
 
 }
